facfc82c0721f995364b3f0a2ff524b108c51d84
MinSeob Kim <64528545+MSKim0215@users.noreply.github.com>
2023-04-02 17:28:59 +0900

    Update 거스름돈.cc

--- Greedy/거스름돈/거스름돈.cc
+++ Greedy/거스름돈/거스름돈.cc
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int coins[] = {500, 100, 50, 10};
+
+    int N;      // 거스름돈
+    cin >> N;
+    
+    int resultCount = 0;
+    
+    for(int i = 0; i < sizeof(coins)/sizeof(*coins); i++)
+    {
+        resultCount += (N / coins[i]);
+        N = N % coins[i];
+    }
+    
+    cout << resultCount;
+}
